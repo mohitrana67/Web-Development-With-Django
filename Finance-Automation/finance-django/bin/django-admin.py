4403bc9670e319cc48bb9963f5815f955edf5215
Mohit Rana <mohitrana67@gmail.com>
2020-05-13 10:32:55 -0400

    Starting the first djangio project

---
+++ Finance-Automation/finance-django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mohitrana/Desktop/Development/Web-Development-With-Django/Finance-Automation/finance-django/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
